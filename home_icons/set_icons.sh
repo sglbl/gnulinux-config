8e1039509093810febb8e40991117e77e8e24f2f
Süleyman <silo1881@gmail.com>
2026-03-03 13:41:45 +0100

    update transparent svg icons

--- home_icons/set_icons.sh
+++ home_icons/set_icons.sh
@@ -12,11 +12,17 @@ for folder_path in "$HOME_DIR"/*; do
     for ext in svg png; do
         icon_file="$SCRIPT_DIR/$icon_name.$ext"
         if [ -f "$icon_file" ]; then
-            gio set "$folder_path" metadata::custom-icon "file://$icon_file"
+            # gio set "$folder_path" metadata::custom-icon "file://$SCRIPT_DIR/$icon_name.$ext"
+            gio set "$folder_path" metadata::custom-icon "file:///usr/share/icons/ZorinBlue-Dark/512x512/mimetypes/inode-directory.png"
+
+            # Add icon support for Dolphin (using KDE-native config writer)
+            kwriteconfig5 --file "$folder_path/.directory" --group "Desktop Entry" --key "Icon" "$icon_file"
+            kwriteconfig5 --file "$folder_path/.directory" --group "Desktop Entry" --key "Type" "Directory"
+            
             echo "Set icon for $folder -> $icon_name.$ext"
             break
         fi
     done
 done
 
-echo "Done! You may need to restart nautilus to see changes: nautilus -q"
+echo "Done! You may need to restart nautilus or dolphin to see changes: nautilus -q ; killall dolphin"
